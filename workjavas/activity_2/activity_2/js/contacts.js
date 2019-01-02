0c55567bf1c92fe5f0872c58777faea0b7536e29
bizwioscar <43906677+bizwioscar@users.noreply.github.com>
2019-01-02 18:35:34 +0200

    Add files via upload

---
+++ workjavas/activity_2/activity_2/js/contacts.js
@@ -0,0 +1,52 @@
+/* 
+Activity: Contact manager
+*/
+
+// TODO: Complete the program
+
+
+console.log("Welcome to your contacts manager!");
+console.log("1: List Contacts");
+console.log("2: Add a contact");
+console.log("0: Quit");
+
+var quit = "";
+while (quit !== "0") {
+    quit = prompt("Type any letter or 0 to exit ,1 to list contact ,2 to add contact:");
+    console.log(quit);
+
+if (number = 1) {
+var contacts = {
+	lastName: "Smith",
+	firstName: "Jhon",
+	lastNames: "Jane",
+	firstNames: "Doe",
+
+
+}
+console.log("Here's the list of all your contacts:");
+console.log("Last name: "+contacts.lastName+ ",   first name: "+contacts.firstName+"");
+console.log("Last name: "+contacts.lastNames+ ",   first name: "+contacts.firstNames+"");
+console.log("contact Added");
+
+console.log("Welcome to your contacts manager!");
+console.log("1: List Contacts");
+console.log("2: Add a contact");
+console.log("0: Quit");
+
+
+}
+else if (number =2 ){ 
+	 console.log("Here's the list of all your contacts:");
+    var names =['Smith','John','Doe','Jane'];
+    
+    for (var i = 0; i < names.length ; i++) {
+    	console.log("First name",names[i] ,",","", "", "Last name");
+
+    }
+
+	console.log("contact Added");
+}
+}
+
+
